21f2f20b2b0e34dcd2a904ed30fed9db1f6ee0e5
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-05 17:14:28 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App: FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div style={{ height: '500px' }}>page</div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;

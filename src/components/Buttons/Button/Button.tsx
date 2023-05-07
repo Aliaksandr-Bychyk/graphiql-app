5ac196a8ac7df79dedfbcebfc8d7641eba6e9a59
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-07 16:42:57 +0300

    feat: add classname props to btn

--- src/components/Buttons/Button/Button.tsx
+++ src/components/Buttons/Button/Button.tsx
@@ -3,11 +3,14 @@ import './Button.scss';
 
 interface IButtonProps {
   onClick?: () => void;
+  className?: string;
 }
 
-const Button: FC<PropsWithChildren<IButtonProps>> = ({ children, onClick }) => {
+const Button: FC<PropsWithChildren<IButtonProps>> = ({ children, onClick, className }) => {
+  const classes = `${'button'} ${className}`;
+
   return (
-    <button className="button" onClick={onClick}>
+    <button className={classes} onClick={onClick}>
       {children}
     </button>
   );

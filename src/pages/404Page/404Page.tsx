440b3b7c45261a2541db25a7ae943d871bc18e85
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-07 16:41:28 +0300

    feat: add 404 page title and subtitle

---
+++ src/pages/404Page/404Page.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import Button from '../../components/Buttons/Button/Button';
+import './404Page.scss';
+
+const NorFoundPage: FC = () => {
+  return (
+    <div className="not-found">
+      <div className="container">
+        <h1 className="not-found__title">404</h1>
+        <h2 className="not-found__subtitle">Oops! The page can&apos;t be found</h2>
+        <Button>Back to the Welcome Page</Button>
+      </div>
+    </div>
+  );
+};
+
+export default NorFoundPage;

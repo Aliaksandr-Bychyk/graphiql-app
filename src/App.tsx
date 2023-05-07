2f6ed0720261ca3a5c4099f0e7ed0e18042adb25
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-07 16:41:28 +0300

    feat: add 404 page title and subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import { FC } from 'react';
+import NorFoundPage from './pages/404Page/404Page';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
-const App: FC = () => <h1>Hello RS School!</h1>;
+const App: FC = () => {
+  return (
+    <>
+      <Header />
+      <NorFoundPage />
+      <Footer />
+    </>
+  );
+};
 
 export default App;

91be41efca8b28a2e7f768143a5cc92789de7d82
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-08 17:11:00 +0300

    feat: add conditional rendering

--- src/pages/AuthRegPage/AuthRegPage.tsx
+++ src/pages/AuthRegPage/AuthRegPage.tsx
@@ -1,13 +1,32 @@
 import { FC } from 'react';
 import TextInput from '../../components/Inputs/TextInput/TextInput';
-import Button from '../../components/Buttons/Button/Button';
 import './AuthRegPage.scss';
 
 const AuthRegPage: FC = () => {
+  const isReg = true;
+
   return (
     <div className="auth-reg">
-      <TextInput type="email" placehilder="Email..." />
-      <TextInput type="text" placehilder="Password..." />
+      <div className="container">
+        {isReg ? (
+          <div className="auth-reg__wrapper">
+            <h1 className="auth-reg__title">Sign Up</h1>
+            <form className="reg__form">
+              <TextInput type="email" placehilder="Email..." />
+              <TextInput type="text" placehilder="Password..." />
+            </form>
+            <div className="reg__question">Do you already have an account? Sing In.</div>
+          </div>
+        ) : (
+          <div className="auth-reg__wrapper">
+            <h1 className="auth-reg__title">Sign In</h1>
+            <form className="auth__form">
+              <TextInput type="email" placehilder="Email..." />
+              <TextInput type="text" placehilder="Password..." />
+            </form>
+          </div>
+        )}
+      </div>
     </div>
   );
 };

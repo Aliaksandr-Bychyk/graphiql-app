fa1ebb6aca8bed948e913b8e106579d4a3d1d404
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-09 13:29:19 +0300

    feat: set up validation

--- src/components/Inputs/TextInput/TextInput.tsx
+++ src/components/Inputs/TextInput/TextInput.tsx
@@ -1,14 +1,40 @@
 import { FC } from 'react';
+import { FieldErrors, UseFormRegister } from 'react-hook-form';
+import { FormInputs } from '../../../pages/AuthRegPage/AuthRegPage';
 import './TextInput.scss';
 
 interface ITextInputProps {
   type: string;
   placehilder: string;
+  register: UseFormRegister<FormInputs>;
+  errors: FieldErrors;
 }
 
-const TextInput: FC<ITextInputProps> = ({ type, placehilder }) => {
+const regExp = new RegExp('(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[-#$.%&@!+=<>*])');
+
+const TextInput: FC<ITextInputProps> = ({ type, placehilder, register, errors }) => {
   return (
-    <input type={type} className="text-input" id={type} name={type} placeholder={placehilder} />
+    <label htmlFor={type}>
+      <input
+        type={type}
+        className="text-input"
+        id={type}
+        // name={type}
+        placeholder={placehilder}
+        {...register(type, {
+          required: 'Value is required',
+          minLength: { value: 8, message: 'Value should be at least 8 chars' },
+          validate: (value: string) =>
+            value.match(regExp) ||
+            'Value should contain at least 1 letter, 1 digit, 1 special char',
+        })}
+      />
+      {errors[type] && (
+        <p role="alert" className="text-input__error">
+          {errors[type]?.message?.toString()}
+        </p>
+      )}
+    </label>
   );
 };
 
--- src/pages/AuthRegPage/AuthRegPage.tsx
+++ src/pages/AuthRegPage/AuthRegPage.tsx
@@ -1,13 +1,28 @@
 import { FC, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import TextInput from '../../components/Inputs/TextInput/TextInput';
 import SubmitInput from '../../components/Inputs/SubmitInput/SubmitInput';
 import './AuthRegPage.scss';
 
+export interface FormInputs {
+  email: string;
+  password: string;
+}
+
 const AuthRegPage: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+  });
+
   const [isReg, setIsReg] = useState<boolean>(true);
 
-  const renderSignIn = () => {
-    setIsReg(false);
+  const onSubmit = () => {
+    //do something
   };
 
   return (
@@ -16,14 +31,19 @@ const AuthRegPage: FC = () => {
         {isReg ? (
           <div className="auth-reg__wrapper">
             <h1 className="auth-reg__title">Sign Up</h1>
-            <form className="auth-reg__form">
-              <TextInput type="email" placehilder="Email..." />
-              <TextInput type="text" placehilder="Password..." />
-              <SubmitInput disabled={true} />
+            <form className="auth-reg__form" onSubmit={handleSubmit(onSubmit)}>
+              <TextInput type="email" placehilder="Email..." register={register} errors={errors} />
+              <TextInput
+                type="text"
+                placehilder="Password..."
+                register={register}
+                errors={errors}
+              />
+              <SubmitInput disabled={false} />
             </form>
             <div className="auth-reg__question">
               Do you already have an account?{' '}
-              <span className="auth-reg__link" onClick={renderSignIn}>
+              <span className="auth-reg__link" onClick={() => setIsReg(false)}>
                 Sing In
               </span>
             </div>
@@ -31,9 +51,14 @@ const AuthRegPage: FC = () => {
         ) : (
           <div className="auth-reg__wrapper">
             <h1 className="auth-reg__title">Sign In</h1>
-            <form className="auth-reg__form">
-              <TextInput type="email" placehilder="Email..." />
-              <TextInput type="text" placehilder="Password..." />
+            <form className="auth-reg__form" onSubmit={handleSubmit(onSubmit)}>
+              <TextInput type="email" placehilder="Email..." register={register} errors={errors} />
+              <TextInput
+                type="text"
+                placehilder="Password..."
+                register={register}
+                errors={errors}
+              />
               <SubmitInput disabled={true} />
             </form>
           </div>

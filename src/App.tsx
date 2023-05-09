6917d13efe181e00668c16d07db3ec7c186df21e
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-09 15:35:01 +0300

    fix: remove page from app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import AuthRegPage from './/pages/AuthRegPage/AuthRegPage';
 
 const App: FC = () => (
   <>
     <Header />
-    <AuthRegPage />
     <Footer />
   </>
 );

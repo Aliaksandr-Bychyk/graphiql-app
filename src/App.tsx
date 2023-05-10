c4c5ac88bf9dcdacaa2eca6654f515f0af381ba1
NursultanFront <nursultan020395@gmail.com>
2023-05-10 14:53:24 +0500

    feat: auth status

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { isUser } from './services/firebase';
 
-const App: FC = () => (
-  <>
-    <Header />
-    <Footer />
-  </>
-);
+const App: FC = () => {
+  useEffect(() => {
+    isUser();
+
+    return () => isUser();
+  });
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,6 +4,7 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   signOut,
+  onAuthStateChanged,
 } from 'firebase/auth';
 
 export const firebaseConfig = {
@@ -42,4 +43,15 @@ export function logOut() {
     .catch((error) => {
       console.log(error);
     });
+}
+
+export function isUser() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log('зашел');
+      // ...
+    } else {
+      console.log('вышел');
+    }
+  });
 }

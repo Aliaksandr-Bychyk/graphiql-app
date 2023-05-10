657f0e01ddc0f7678cde2bdb23d45bce0d6668fc
Aliaksandr <sanya55bychyk@gmail.com>
2023-05-10 22:51:15 +0200

    feat: add react-router

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,13 @@
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
-interface ILayoutProps {
-  page: ReactNode;
-}
-
-const Layout: FC<ILayoutProps> = ({ page }) => {
+const Layout: FC = () => {
   return (
     <>
       <Header />
-      {page}
+      <Outlet />
       <Footer />
     </>
   );

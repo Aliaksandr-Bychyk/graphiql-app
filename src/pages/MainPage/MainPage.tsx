16684221e1529490b357d8d307d32a7d324ee262
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-11 18:37:19 +0300

    feat: create main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,11 +4,24 @@ import './MainPage.scss';
 
 const MainPage: FC = () => {
   const [operationValue, setOperationValue] = useState<string>('');
+  const [variablesValue, setVariablesValue] = useState<string>('');
+  const [headersValue, setHeadersValue] = useState<string>('');
+
+  const [isVar, setIsVar] = useState<boolean>(true);
 
   return (
     <div className="main">
-      <div className="container">
-        <Editor title={'Operation'} onChange={setOperationValue} value={operationValue} />
+      <div className="container main__container">
+        <div className="main__documentation">documentation</div>
+        <div className="main__editors">
+          <Editor title={'Operation'} onChange={setOperationValue} value={operationValue} />
+          {isVar ? (
+            <Editor title={'Variables'} onChange={setVariablesValue} value={variablesValue} />
+          ) : (
+            <Editor title={'Headers'} onChange={setHeadersValue} value={headersValue} />
+          )}
+        </div>
+        <div className="main__response">response</div>
       </div>
     </div>
   );

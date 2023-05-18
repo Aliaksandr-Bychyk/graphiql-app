ba3f44338f1ba8c9ff7d6c2bb59f41df3227b85a
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-05-18 13:09:14 +0300

    fix: remove main page import

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Layout from './components/Layout/Layout';
 import WelcomePage from './pages/WelcomePage/WelcomePage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import AuthRegPage from './pages/AuthRegPage/AuthRegPage';
-import MainPage from './pages/MainPage/MainPage';
 
 const App: FC = () => {
   return (
@@ -14,7 +13,6 @@ const App: FC = () => {
           <Route index element={<WelcomePage />} />
           <Route path="sign-up" element={<AuthRegPage />} />
           <Route path="sign-in" element={<AuthRegPage />} />
-          <Route path="main" element={<MainPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>

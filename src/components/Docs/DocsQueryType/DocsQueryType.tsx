44bbf1da156ffb13d2c2ddccea1f47703c04f770
Aliaksandr <sanya55bychyk@gmail.com>
2023-05-22 22:00:40 +0200

    feat: add DocsQueryType component

---
+++ src/components/Docs/DocsQueryType/DocsQueryType.tsx
@@ -0,0 +1,23 @@
+import { IQueryType } from '@/interfaces/Docs';
+import { FC, useContext } from 'react';
+import { DocsContext } from '../DocsWindow/DocsWindow';
+
+interface IDocsQueryTypeProps {
+  type: IQueryType;
+}
+
+const DocsQueryType: FC<IDocsQueryTypeProps> = ({ type }) => {
+  const { value, setValue } = useContext(DocsContext);
+  return (
+    <span
+      onClick={() => {
+        console.log(type);
+        setValue!(type);
+      }}
+    >
+      {type.name}
+    </span>
+  );
+};
+
+export default DocsQueryType;

45cb461205b9bc11277c2e611daa52f03e5f7eee
Aliaksandr <sanya55bychyk@gmail.com>
2023-05-28 21:43:26 +0200

    feat: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  define: {
-    global: 'window',
   },
 });
